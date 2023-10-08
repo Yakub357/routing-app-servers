33c493715b1e35a5d3349d155b234a3d605078f1
Coding Raccoon <egamyakub357@gmail.com>
2023-10-08 16:38:01 +0800

    retrieving query params and fragments, practicing and gotchas

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
